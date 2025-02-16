2bad7692ddd24ab2ccac14fd8b0a6e8c139d5c54
Isaac Novaes <isaac.novaes@hotmail.com>
2025-02-16 12:20:08 +0100

    Finish linters and formatters

--- eslint.config.js
+++ eslint.config.js
@@ -1,40 +1,17 @@
 import eslintPlugin from '@eslint/js';
-import globals from 'globals';
-import reactHooksPlugin from 'eslint-plugin-react-hooks';
-import reactRefreshPlugin from 'eslint-plugin-react-refresh';
-import tseslintPlugin from 'typescript-eslint';
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import reactPlugin from 'eslint-plugin-react';
-import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import reactRefreshPlugin from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslintPlugin from 'typescript-eslint';
 
 export default tseslintPlugin.config(
-    eslintPlugin.configs.recommended,
-    tseslintPlugin.configs.strict,
-    tseslintPlugin.configs.stylistic,
-    eslintPluginPrettierRecommended,
-    reactPlugin.configs.flat.recommended,
-    reactPlugin.configs.flat['jsx-runtime'],
-    jsxA11yPlugin.flatConfigs.strict,
-    { ignores: ['dist'] },
     {
-        files: ['**/*.{ts,tsx}'],
-        languageOptions: {
-            ecmaVersion: '2023',
-            globals: globals.browser,
-            parserOptions: {
-                projectService: true,
-                tsconfigRootDir: import.meta.dirname,
-                ecmaFeatures: {
-                    jsx: true,
-                },
-            },
-        },
-        plugins: {
-            'react-hooks': reactHooksPlugin,
-            'react-refresh': reactRefreshPlugin,
-            react: reactPlugin,
-        },
+        languageOptions: { globals: globals.browser },
         rules: {
+            ...eslintPlugin.configs.recommended.rules,
             'array-callback-return': 'warn',
             'no-duplicate-imports': ['warn', { includeExports: true }],
             'no-self-compare': 'error',
@@ -45,9 +22,7 @@ export default tseslintPlugin.config(
             'default-case-last': 'warn',
             'guard-for-in': 'error',
             'no-console': 'warn',
-            'no-empty-function': 'error',
             'no-lone-blocks': 'error',
-            'no-magic-numbers': ['warn', { enforceConst: true, detectObjects: true }],
             'no-param-reassign': 'error',
             'no-else-return': ['error', { allowElseIf: false }],
             'no-extra-boolean-cast': ['error', { enforceForInnerExpressions: true }],
@@ -59,39 +34,96 @@ export default tseslintPlugin.config(
             'no-var': 'error',
             'object-shorthand': 'warn',
             'prefer-const': 'error',
-            'prefer-destructuring': 'error',
             'prefer-object-has-own': 'error',
             'prefer-template': 'error',
-            'require-await': 'warn',
-            'sort-imports': 'warn',
-            'sort-keys': ['warn', 'asc', { natural: true }],
-            'sort-vars': 'warn',
             yoda: 'error',
             camelcase: 'error',
-            ...reactHooksPlugin.configs.recommended.rules,
-            '@typescript-eslint/consistent-type-definitions': 'off',
+        },
+    },
+    eslintPluginPrettierRecommended,
+    reactPlugin.configs.flat.recommended,
+    reactPlugin.configs.flat['jsx-runtime'],
+    jsxA11yPlugin.flatConfigs.strict,
+    ...tseslintPlugin.configs.recommendedTypeChecked.map((config) => ({
+        ...config,
+        files: ['**/*.{ts,tsx}'], // We use TS config only for TS files
+    })),
+    ...tseslintPlugin.configs.strictTypeChecked.map((config) => ({
+        ...config,
+        files: ['**/*.{ts,tsx}'], // We use TS config only for TS files
+    })),
+    {
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2023,
+            globals: globals.browser,
+            parserOptions: {
+                projectService: true,
+                project: ['./tsconfig.node.json', './tsconfig.app.json'],
+                tsconfigRootDir: import.meta.dirname,
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+        settings: { react: { version: '19.0.0' } },
+        plugins: {
+            'react-hooks': reactHooksPlugin,
+            'react-refresh': reactRefreshPlugin,
+            react: reactPlugin,
+        },
+        rules: {
+            // Typescript ---------------------------------------------------------------
+
             '@typescript-eslint/consistent-type-exports': 'warn',
             '@typescript-eslint/consistent-type-imports': 'warn',
             '@typescript-eslint/method-signature-style': 'warn',
-            '@typescript-eslint/naming-convention': [
+            '@typescript-eslint/promise-function-async': 'error',
+            '@typescript-eslint/no-useless-empty-export': 'warn',
+            '@typescript-eslint/no-unsafe-type-assertion': 'error',
+            'require-await': 'off',
+            '@typescript-eslint/require-await': 'error',
+            'no-empty-function': 'off',
+            '@typescript-eslint/no-empty-function': 'error',
+            'prefer-destructuring': 'off',
+            '@typescript-eslint/prefer-destructuring': 'error',
+            'no-magic-numbers': 'off',
+            '@typescript-eslint/no-magic-numbers': [
                 'warn',
                 {
-                    selector: 'function',
-                    format: ['PascalCase', 'camelCase'],
+                    enforceConst: true,
+                    detectObjects: true,
+                    ignoreEnums: true,
+                    ignoreNumericLiteralTypes: true,
+                    ignoreReadonlyClassProperties: true,
                 },
-            ], // avoid react component naming warning
-            '@typescript-eslint/no-redundant-type-constituents': 'warn',
-            '@typescript-eslint/no-useless-empty-export': 'warn',
+            ],
             'no-redeclare': 'off',
-            '@typescript-eslint/no-redeclare': ['error'], // overwrite base no-redeclare eslint
+            '@typescript-eslint/no-redeclare': ['error'],
             'no-shadow': 'off',
-            '@typescript-eslint/no-shadow': ['warn', { hoist: 'all' }], // overwrite base no-shadow eslint
+            '@typescript-eslint/no-shadow': ['warn', { hoist: 'all' }],
             'no-unused-expressions': 'off',
-            '@typescript-eslint/no-unused-expressions': ['error'], // overwrite base no-unused-expressions eslint
+            '@typescript-eslint/no-unused-expressions': 'error',
             'no-use-before-define': 'off',
-            '@typescript-eslint/no-use-before-define': 'warn', // overwrite base no-use-before-define eslint
-            '@typescript-eslint/no-unused-vars': 'off', // ts already does it
+            '@typescript-eslint/no-use-before-define': 'warn',
+            'no-unused-vars': 'off',
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    args: 'all',
+                    argsIgnorePattern: '^_',
+                    caughtErrors: 'all',
+                    caughtErrorsIgnorePattern: '^_',
+                    destructuredArrayIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                    ignoreRestSiblings: true,
+                },
+            ],
+
+            // React ------------------------------------------------------
+
             'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+            ...reactHooksPlugin.configs.recommended.rules,
             'react/boolean-prop-naming': ['warn', { validateNested: true }],
             'react/button-has-type': 'warn',
             'react/destructuring-assignment': [
@@ -105,19 +137,27 @@ export default tseslintPlugin.config(
                 'warn',
                 { checkLocalVariables: true, checkInlineFunction: false },
             ],
+            'react/jsx-no-constructed-context-values': 'warn',
             'react/jsx-no-leaked-render': ['warn', { validStrategies: ['ternary', 'coerce'] }],
             'react/jsx-no-useless-fragment': 'warn',
             'react/jsx-sort-props': [
                 'warn',
                 { ignoreCase: true, callbacksLast: true, reservedFirst: ['key', 'ref'] },
             ],
+
             'react/no-array-index-key': 'warn',
             'react/no-unstable-nested-components': 'error',
             'react/no-unused-prop-types': 'warn',
             'react/prefer-read-only-props': 'warn',
             'react/self-closing-comp': 'warn',
-            'react/jsx-no-constructed-context-values': 'warn',
             'react/style-prop-object': 'warn',
+            '@typescript-eslint/naming-convention': [
+                'warn',
+                {
+                    selector: 'function',
+                    format: ['PascalCase', 'camelCase'],
+                },
+            ], // avoid react component naming warning
         },
     }
 );
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById('root');
+
+if (!root) {
+    throw new ReferenceError('There is no root element');
+}
+
+createRoot(root).render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);

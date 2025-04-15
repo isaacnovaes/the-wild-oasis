68ae9e053edc00f840188adbf69138f2df18572b
Isaac Novaes <isaac.novaes@hotmail.com>
2025-04-15 13:31:01 +0200

    Add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
 function App() {
-    return <div>Hello</div>;
+    return (
+        <div className='group grid h-dvh place-items-center bg-indigo-200 has-focus:bg-red-500'>
+            <button
+                className='text-center hover:cursor-pointer focus-visible:bg-amber-600'
+                type='button'
+            >
+                Hello world
+            </button>
+            <a
+                className='group-hover:bg-blend-lighten'
+                href='https://google.com'
+                rel='noopener noreferrer'
+                target='_blank'
+            >
+                Hello
+            </a>
+        </div>
+    );
 }
 
 export default App;

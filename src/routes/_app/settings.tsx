7af7d5d0695d329f346e2b201170dba53893cdee
Isaac Novaes <isaac.novaes@hotmail.com>
2025-05-16 11:27:27 +0200

    Update routes

--- src/routes/_app/settings.tsx
+++ src/routes/_app/settings.tsx
@@ -1,6 +1,13 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, ErrorComponent } from '@tanstack/react-router';
+import Loading from '../../components/Loading';
 import Settings from '../../features/settings/Settings';
+import { settingsQueryOptions } from '../../utils/queryOptions';
 
 export const Route = createFileRoute('/_app/settings')({
     component: Settings,
+    loader: async ({ context: { queryClient } }) => {
+        await queryClient.prefetchQuery(settingsQueryOptions);
+    },
+    errorComponent: ErrorComponent,
+    pendingComponent: Loading,
 });

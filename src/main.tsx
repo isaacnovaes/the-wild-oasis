16ec98dd12d2a57eb34f06e4a35308101437ee94
Isaac Novaes <isaac.novaes@hotmail.com>
2025-05-16 11:27:40 +0200

    Update main

--- src/main.tsx
+++ src/main.tsx
@@ -12,9 +12,39 @@ if (!root) {
     throw new ReferenceError('There is no root element');
 }
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 1000 * 60 * 20, // 5 minutes
+            gcTime: Infinity,
+            retry: false,
+        },
+    },
+});
 
-const router = createRouter({ routeTree, context: { queryClient } });
+const router = createRouter({
+    routeTree,
+    context: { queryClient },
+});
+
+type QueryKeys =
+    | 'account'
+    | 'booking'
+    | 'bookings'
+    | 'dashboard'
+    | 'cabins'
+    | 'checkin'
+    | 'settings'
+    | 'users';
+
+type QueryKey = [QueryKeys, ...ReadonlyArray<unknown>];
+
+declare module '@tanstack/react-query' {
+    interface Register {
+        queryKey: QueryKey;
+        mutationKey: QueryKey;
+    }
+}
 
 declare module '@tanstack/react-router' {
     interface Register {
@@ -26,7 +56,6 @@ createRoot(root).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
             <RouterProvider context={{ queryClient }} router={router} />
-
             <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
     </StrictMode>

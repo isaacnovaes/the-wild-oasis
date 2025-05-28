f162d59ceeb5be46477057803173f2c3d580a854
Isaac Novaes <isaac.novaes@hotmail.com>
2025-05-28 19:44:13 +0200

    Update account component

--- src/features/account/Account.tsx
+++ src/features/account/Account.tsx
@@ -1,13 +1,11 @@
 import PageHeader from '@/components/PageHeader';
 import UpdateUserDataForm from './UpdateUserDataForm';
-import UpdateUserPasswordForm from './UpdateUserPasswordForm';
 
 const Account = () => {
     return (
         <>
             <PageHeader title='Manage your account' />
             <UpdateUserDataForm />
-            <UpdateUserPasswordForm />
         </>
     );
 };
